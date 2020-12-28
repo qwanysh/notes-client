b132ef2a511a31a581941d6fd259719da3c1cc67
qwanysh <kuanysh.beisembayev@gmail.com>
2020-12-28 11:10:13 +0600

    Add home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,15 @@ const Header = () => {
   return (
     <header className="py-4">
       <div className="container is-flex is-justify-content-space-between is-align-items-center px-4">
-        <div className="title has-text-dark is-6 mb-0">qwanysh's notes</div>
+        <div className="is-flex is-align-items-center">
+          <span class="icon is-small mr-1">
+            <i class="fas fa-sticky-note"></i>
+          </span>
+          <div className="title is-6 mb-0">Заметочки</div>
+        </div>
         <div className="is-flex">
-          <a className="button is-rounded is-warning" href="test">
-            sign in
+          <a className="button is-rounded is-warning" href="#">
+            Войти
           </a>
         </div>
       </div>
