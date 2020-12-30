d559d8e72cad6f5bd66577a5c6e27c802e13d973
qwanysh <olimpsdu@gmail.com>
2020-12-30 11:28:02 +0600

    Configure redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import reducer from './store/rootReducer';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
